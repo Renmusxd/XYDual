e5091ff9066fd08a093037e18e31655e9fe806dd
Sumner Hearth <sumnernh@gmail.com>
2023-01-17 20:05:51 -0500

    Update threading

--- src/lattice.rs
+++ src/lattice.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array1, Array2};
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2, ArrayViewMut2};
 use num_traits::float::Float;
 use rand::Rng;
 use rayon::iter::ParallelIterator;
@@ -12,7 +12,7 @@ pub struct Lattice {
     b_lattice: Array2<i32>,
     boundaries: (i32, i32),
     potential: Array1<f64>,
-    check_range: usize,
+    mc_trials: [i32; 3],
 }
 
 impl Index<&(usize, usize)> for Lattice {
@@ -58,7 +58,7 @@ impl Lattice {
             b_lattice: Array2::zeros((x, y / 2)),
             boundaries: (0, 0),
             potential: potential.into(),
-            check_range: 1,
+            mc_trials: [-1, 0, 1],
         }
     }
 
@@ -190,16 +190,194 @@ impl Lattice {
         out
     }
 
-    pub fn update(&mut self) {
-        self.update_a();
-        self.update_b();
+    #[inline]
+    fn a_neighbors(ax: usize, ay: usize, lx: usize, ly: usize) -> [((usize, usize), (i8, i8)); 4] {
+        let aly = ly / 2;
+        let col_parity = ax % 2;
+
+        // -1 x
+        let mx = ((ax + lx - 1) % lx, ay);
+        let wmx = (if ax == 0 { -1 } else { 0 }, 0);
+        // +1 x
+        let px = ((ax + lx + 1) % lx, ay);
+        let wpx = (if ax + 1 == lx { 1 } else { 0 }, 0);
+
+        if col_parity == 0 {
+            // -1 y
+            let my = (ax, (ay + aly - 1) % aly);
+            let wmy = (0, if ay == 0 { -1 } else { 0 });
+            // +1 y (can't go off top on a 0 parity column)
+            let py = (ax, ay);
+            let wpy = (0, 0);
+
+            [(mx, wmx), (px, wpx), (my, wmy), (py, wpy)]
+        } else {
+            // -1 y (can't go off bottom on a 1 parity column)
+            let my = (ax, ay);
+            let wmy = (0, 0);
+            // +1 y
+            let py = (ax, (ay + aly + 1) % aly);
+            let wpy = (0, if ay + 1 == aly { 1 } else { 0 });
+            [(mx, wmx), (px, wpx), (my, wmy), (py, wpy)]
+        }
     }
-    fn update_a(&mut self) {
+
+    #[inline]
+    fn b_neighbors(bx: usize, by: usize, lx: usize, ly: usize) -> [((usize, usize), (i8, i8)); 4] {
+        let bly = ly / 2;
+        let col_parity = bx % 2;
+
+        // -1 x
+        let mx = ((bx + lx - 1) % lx, by);
+        let wmx = (if bx == 0 { -1 } else { 0 }, 0);
+        // +1 x
+        let px = ((bx + lx + 1) % lx, by);
+        let wpx = (if bx + 1 == lx { 1 } else { 0 }, 0);
+
+        if col_parity == 0 {
+            // -1 y (can't go off bottom on a 1 parity column)
+            let my = (bx, by);
+            let wmy = (0, 0);
+            // +1 y
+            let py = (bx, (by + bly + 1) % bly);
+            let wpy = (0, if by + 1 == bly { 1 } else { 0 });
+            [(mx, wmx), (px, wpx), (my, wmy), (py, wpy)]
+        } else {
+            // -1 y
+            let my = (bx, (by + bly - 1) % bly);
+            let wmy = (0, if by == 0 { -1 } else { 0 });
+            // +1 y (can't go off top on a 0 parity column)
+            let py = (bx, by);
+            let wpy = (0, 0);
+
+            [(mx, wmx), (px, wpx), (my, wmy), (py, wpy)]
+        }
+    }
+
+    pub fn new_update<R: Rng>(&mut self, rng: &mut R) {
+        self.new_update_a(rng);
+        self.new_update_b(rng);
+    }
+
+    fn new_update_a<R: Rng>(&mut self, rng: &mut R) {
+        let alpha_lat = self.a_lattice.view_mut();
+        let beta_lat = self.b_lattice.view();
+        let pots = self.potential.view();
+        let winding = self.boundaries;
+        let mc_trials = &self.mc_trials;
+
+        Self::update_gen(
+            alpha_lat,
+            beta_lat,
+            pots,
+            winding,
+            mc_trials,
+            Self::a_neighbors,
+            rng,
+        )
+    }
+
+    fn new_update_b<R: Rng>(&mut self, rng: &mut R) {
+        let alpha_lat = self.b_lattice.view_mut();
+        let beta_lat = self.a_lattice.view();
+        let pots = self.potential.view();
+        let winding = self.boundaries;
+        let mc_trials = &self.mc_trials;
+
+        Self::update_gen(
+            alpha_lat,
+            beta_lat,
+            pots,
+            winding,
+            mc_trials,
+            Self::b_neighbors,
+            rng,
+        )
+    }
+
+    pub fn update_gen<FN, R: Rng, const N: usize>(
+        mut alpha_lat: ArrayViewMut2<i32>,
+        beta_lat: ArrayView2<i32>,
+        pots: ArrayView1<f64>,
+        winding: (i32, i32),
+        mc_trials: &[i32; N],
+        neighbors: FN,
+        rng: &mut R,
+    ) where
+        FN: Fn(usize, usize, usize, usize) -> [((usize, usize), (i8, i8)); 4] + Send + Sync,
+    {
+        let (wx, wy) = winding;
+        let lx = alpha_lat.shape()[0];
+        let ly = 2 * alpha_lat.shape()[1];
+        ndarray::Zip::indexed(&mut alpha_lat).for_each(|(alpha_x, alpha_y), val| {
+            let beta_neighbors = neighbors(alpha_x, alpha_y, lx, ly);
+
+            let mut neighbor_diffs = [0; 4];
+            neighbor_diffs
+                .iter_mut()
+                .zip(beta_neighbors.into_iter())
+                .for_each(|(v, ((beta_x, beta_y), (wrap_x, wrap_y)))| {
+                    let beta_v = beta_lat
+                        .get((beta_x, beta_y))
+                        .copied()
+                        .expect("Index not found");
+                    let winding_num = (wrap_x as i32 * wx) + (wrap_y as i32 * wy);
+                    *v = *val - beta_v + winding_num;
+                });
+            let mut weights = [0.0; N];
+            weights
+                .iter_mut()
+                .zip(mc_trials.into_iter())
+                .for_each(|(w, dv)| {
+                    *w = neighbor_diffs
+                        .iter()
+                        .map(|nv| nv + dv)
+                        .map(|v| {
+                            pots.get([v.abs() as usize])
+                                .copied()
+                                .unwrap_or(f64::INFINITY)
+                        })
+                        .sum::<f64>();
+                });
+            let min_e = weights
+                .iter()
+                .copied()
+                .fold(f64::infinity(), |acc, x| acc.min(x));
+            weights.iter_mut().for_each(|x| *x = (-(*x - min_e)).exp());
+
+            // Probs is now the set of boltzman weights for each of the dvs
+            let total_weight = weights.iter().sum::<f64>();
+            // Probs is now the acceptance probabilities for each of the dvs
+
+            let choice = rng.gen::<f64>() * total_weight;
+            let dv = weights
+                .into_iter()
+                .zip(mc_trials.iter().copied())
+                .try_fold(choice, |mut acc, (w, dv)| {
+                    acc -= w;
+                    if acc <= 0.0 {
+                        Err(dv)
+                    } else {
+                        Ok(acc)
+                    }
+                })
+                .unwrap_err();
+            *val += dv;
+        });
+    }
+
+    pub fn old_update(&mut self) {
+        self.old_update_a();
+        self.old_update_b();
+    }
+
+    fn old_update_a(&mut self) {
         let (wx, wy) = self.boundaries;
         let lx = self.a_lattice.shape()[0];
         let ly = 2 * self.a_lattice.shape()[1];
         let b_sub = self.b_lattice.view();
         let pots = self.potential.view();
+        let dvs = &self.mc_trials;
         ndarray::Zip::indexed(&mut self.a_lattice)
             .into_par_iter()
             .for_each(|((ix, iy), val)| {
@@ -217,9 +395,9 @@ impl Lattice {
                         let winding_num = (wrap_x as i32 * wx) + (wrap_y as i32 * wy);
                         *v = *b_sub.get((bx, by)).expect("neighbor not found") + winding_num;
                     });
-                let dvs = -(self.check_range as i32)..=self.check_range as i32;
                 let mut weights = dvs
-                    .clone()
+                    .iter()
+                    .copied()
                     .map(|dv| {
                         let nv = v + dv;
                         neighbors
@@ -243,7 +421,7 @@ impl Lattice {
                 let choice = rng.gen::<f64>() * total_weight;
                 let dv = weights
                     .into_iter()
-                    .zip(dvs)
+                    .zip(dvs.iter().copied())
                     .try_fold(choice, |mut acc, (w, dv)| {
                         acc -= w;
                         if acc <= 0.0 {
@@ -256,12 +434,13 @@ impl Lattice {
                 *val += dv;
             });
     }
-    fn update_b(&mut self) {
+    fn old_update_b(&mut self) {
         let (wx, wy) = self.boundaries;
         let lx = self.a_lattice.shape()[0];
         let ly = 2 * self.a_lattice.shape()[1];
         let a_sub = self.a_lattice.view();
         let pots = self.potential.view();
+        let dvs = &self.mc_trials;
         ndarray::Zip::indexed(&mut self.b_lattice)
             .into_par_iter()
             .for_each(|((ix, iy), val)| {
@@ -279,9 +458,9 @@ impl Lattice {
                         let winding_num = (wrap_x as i32 * wx) + (wrap_y as i32 * wy);
                         *v = *a_sub.get((ax, ay)).expect("neighbor not found") + winding_num;
                     });
-                let dvs = -(self.check_range as i32)..=self.check_range as i32;
                 let mut weights = dvs
-                    .clone()
+                    .iter()
+                    .copied()
                     .map(|dv| {
                         let nv = v + dv;
                         neighbors
@@ -304,7 +483,7 @@ impl Lattice {
                 let choice = rng.gen::<f64>() * total_weight;
                 let dv = weights
                     .into_iter()
-                    .zip(dvs)
+                    .zip(dvs.iter().copied())
                     .try_fold(choice, |mut acc, (w, dv)| {
                         acc -= w;
                         if acc <= 0.0 {
@@ -321,31 +500,27 @@ impl Lattice {
         let (wx, wy) = self.boundaries;
         let lx = self.a_lattice.shape()[0];
         let ly = 2 * self.a_lattice.shape()[1];
-        ndarray::Zip::indexed(&self.a_lattice)
-            .into_par_iter()
-            .map(|((ax, ay), v)| {
-                let (x, y) = Self::a_to_global(ax, ay);
-                let positive_neighbors = Self::neighbors(x, y, lx, ly);
-                positive_neighbors
-                    .into_iter()
-                    .map(|(n, (wrap_x, wrap_y))| {
-                        let winding_num = (wrap_x as i32 * wx) + (wrap_y as i32 * wy);
-                        let nv = match n {
-                            SubLatticeIndex::B(x, y) => self
-                                .b_lattice
-                                .get([x, y])
-                                .copied()
-                                .expect("neighbor not found"),
-                            SubLatticeIndex::A(_, _) => unreachable!(),
-                        } + winding_num;
-                        self.potential
-                            .get((nv - v).abs() as usize)
-                            .copied()
-                            .unwrap_or(f64::infinity())
-                    })
-                    .sum::<f64>()
-            })
-            .sum::<f64>()
+        ndarray::Zip::indexed(&self.a_lattice).fold(0.0, |acc, (ax, ay), v| {
+            let b_neighbors = Self::a_neighbors(ax, ay, lx, ly);
+
+            let res = b_neighbors
+                .into_iter()
+                .map(|((bx, by), (wrap_x, wrap_y))| {
+                    let winding_num = (wrap_x as i32 * wx) + (wrap_y as i32 * wy);
+                    let nv = self
+                        .b_lattice
+                        .get([bx, by])
+                        .copied()
+                        .expect("neighbor not found")
+                        + winding_num;
+                    self.potential
+                        .get((nv - v).abs() as usize)
+                        .copied()
+                        .unwrap_or(f64::infinity())
+                })
+                .sum::<f64>();
+            res + acc
+        })
     }
 }
 
@@ -373,6 +548,16 @@ mod tests {
 
         let sub = Lattice::global_to_sub(2, 1);
         assert_eq!(sub, B(2, 0));
+    }
+
+    #[test]
+    fn test_sublat_neighbors() {
+        for i in 0..4 {
+            for j in 0..2 {
+                let res = Lattice::a_neighbors(i, j, 4, 4);
+                println!("{},{}\t{:?}", i, j, res)
+            }
+        }
     }
 
     #[test]
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,46 @@
 mod lattice;
 
 use crate::lattice::Lattice;
-use ndarray::Array1;
-use numpy::{IntoPyArray, PyArray1};
+use ndarray::{Array1, Array2, Axis};
+use numpy::{IntoPyArray, PyArray1, PyArray2};
 use pyo3::prelude::*;
+use rayon::prelude::*;
 
 #[pyclass]
 struct PyLattice {
-    lat: Lattice,
+    lat: Vec<Lattice>,
 }
 #[pymethods]
 impl PyLattice {
     #[new]
-    fn new(lx: usize, ly: usize, pots: Vec<f64>) -> Self {
+    fn new(lx: usize, ly: usize, pots: Vec<f64>, experiments: Option<usize>) -> Self {
+        let experiments = experiments.unwrap_or(1);
         Self {
-            lat: Lattice::new(lx, ly, pots),
+            lat: (0..experiments)
+                .map(|_| Lattice::new(lx, ly, pots.clone()))
+                .collect(),
         }
     }
 
     fn update(&mut self, updates: Option<usize>) {
         let updates = updates.unwrap_or(1);
-        for _ in 0..updates {
-            self.lat.update()
-        }
+
+        self.lat.par_iter_mut().for_each(|lat| {
+            let mut rng = rand::thread_rng();
+            for _ in 0..updates {
+                lat.new_update(&mut rng)
+            }
+        });
     }
 
-    fn get_energy(&self) -> f64 {
-        self.lat.get_total_energy()
+    fn get_energy(&self, py: Python) -> Py<PyArray1<f64>> {
+        let mut energies = Array1::zeros((self.lat.len(),));
+        energies
+            .axis_iter_mut(Axis(0))
+            .into_par_iter()
+            .zip(self.lat.par_iter())
+            .for_each(|(mut e, lat)| *e.get_mut([]).unwrap() = lat.get_total_energy());
+        energies.into_pyarray(py).to_owned()
     }
 
     fn simulate_and_get_energy(
@@ -34,15 +48,21 @@ impl PyLattice {
         py: Python,
         num_samples: usize,
         local_updates_per_step: Option<usize>,
-    ) -> Py<PyArray1<f64>> {
+    ) -> Py<PyArray2<f64>> {
         let local_updates_per_step = local_updates_per_step.unwrap_or(1);
 
-        let mut energy = Array1::<f64>::zeros((num_samples,));
-        energy.iter_mut().for_each(|x| {
-            for _ in 0..local_updates_per_step {
-                self.lat.update();
-            }
-            *x = self.lat.get_total_energy();
+        let mut energy = Array2::<f64>::zeros((num_samples, self.lat.len()));
+        energy.axis_iter_mut(Axis(0)).for_each(|mut x| {
+            x.axis_iter_mut(Axis(0))
+                .into_par_iter()
+                .zip(self.lat.par_iter_mut())
+                .for_each(|(mut e, lat)| {
+                    let mut rng = rand::thread_rng();
+                    for _ in 0..local_updates_per_step {
+                        lat.new_update(&mut rng)
+                    }
+                    *e.get_mut([]).unwrap() = lat.get_total_energy();
+                });
         });
         energy.into_pyarray(py).to_owned()
     }
